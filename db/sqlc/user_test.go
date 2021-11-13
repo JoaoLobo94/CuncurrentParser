170f749644d3c0a17badbf2c34da1dbda3b29fe5
joao <joao.lobom@gmail.com>
2021-11-13 21:03:26 +0000

    test user actions

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -2,18 +2,36 @@ package db
 
 import (
 	"context"
-	"testing"
 	"database/sql"
-	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
 	"github.com/pioz/faker"
+	"github.com/stretchr/testify/require"
 )
 
-func TestCreateUser(t *testing.T){
-	var name = sql.NullString{String: faker.Username(), Valid: true}
+func CreateRandomUser(t *testing.T) User{
+	name := sql.NullString{String: faker.Username(), Valid: true}
 	user, err := testQueries.CreateUser(context.Background(), name)
 	require.NoError(t, err)
 	require.NotEmpty(t, user)
 	require.Equal(t, name, user.Name)
 	require.NotZero(t, user.ID)
 	require.NotZero(t, user.CreatedAt)
+
+	return user
+
+}
+
+func TestCreateUser(t *testing.T){
+	CreateRandomUser(t)
+}
+
+func TestGetUser(t *testing.T){
+	user1 := CreateRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Name, user2.Name)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
 }
