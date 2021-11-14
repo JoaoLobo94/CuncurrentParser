6610763b3573bf28e8a0a3fca163064126673ad7
joao <joao.lobom@gmail.com>
2021-11-14 13:16:43 +0000

    Implement startOver

--- util/logic.go
+++ util/logic.go
@@ -1,10 +1,13 @@
 package util
 
 import (
+	"fmt"
+	"context"
+	"github.com/JoaoLobo94/donut_test/db/sqlc"
 	"github.com/AlecAivazis/survey/v2"
 )
 
-func Prompt() string {
+func Prompt(ctx context.Context, queries *db.Queries) {
 	res := ""
 	prompt := &survey.Select{
 	    Message: "What would you like to do now?",
@@ -17,6 +20,36 @@ func Prompt() string {
 	    "View all transactions sent to the bank"},
 	}
 	survey.AskOne(prompt, &res) 
-	return res
+	fmt.Printf("Ok, lets: " + res)
+
+	switch res {
+	case "Start Over --> This will erase all database":
+		startOver(ctx, queries)
+	case "Start Batching all low volume bank transactions. This will run concurrently":
+		startBatch(ctx, queries)
+	case "View all active users":
+		// listUsers()
+	case "View all batches":
+		// listBatches()
+	case "View all dispatched batches":
+		// listDispatchedBatches()
+	case "View all transactions sent to the bank":
+		// listTransactions()
+	default:
+		fmt.Println("Exiting")
+	}
     }
     
+    func startOver(ctx context.Context, queries *db.Queries) {
+	fmt.Printf("Erasing....")
+	queries.DeleteActions(ctx)
+	queries.DeleteBatches(ctx)
+	queries.DeleteTransactions(ctx)
+	queries.DeleteUsers(ctx)
+	fmt.Printf("Erased")
+	Prompt(ctx, queries)
+    }
+
+    func startBatch(ctx context.Context, queries *db.Queries) {
+	    
+    }
